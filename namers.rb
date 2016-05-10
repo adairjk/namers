a03bac4b4b407d690e95c2b54a54da13794bfb0f
Jarod Adair <jarod.adair@manheim.com>
2016-05-10 17:23:40 -0500

    add basic namers class and update tests

---
+++ namers.rb
@@ -0,0 +1,7 @@
+class Namers
+
+  def self.parse_name(name)
+
+  end
+
+end
--- spec/namers_spec.rb
+++ spec/namers_spec.rb
@@ -8,23 +8,23 @@ require_relative '../namers'
 
   RSpec.describe Namers do
     it 'should return a name in the correct order' do
-      expect(parse_name('John Doe')).to eql 'Doe, John'
+      expect(Namers.parse_name('John Doe')).to eql 'Doe, John'
     end
 
     it 'should respect middle names' do
-      expect(parse_name('John James Doe')).to eql 'Doe, John James'
+      expect(Namers.parse_name('John James Doe')).to eql 'Doe, John James'
     end
 
     it 'should respect middle initials' do
-      expect(parse_name('John J Doe')).to eql 'Doe, John J'
+      expect(Namers.parse_name('John J Doe')).to eql 'Doe, John J'
     end
 
     it 'should respect punctuated middle names' do
-      expect(parse_name('John J. Doe')).to eql 'Doe, John J.'
+      expect(Namers.parse_name('John J. Doe')).to eql 'Doe, John J.'
     end
 
     it 'should respect and drop basic honorifics' do
-      expect(parse_name('Dr. John J. Doe')).to eql 'Doe, John J.'
+      expect(Namers.parse_name('Dr. John J. Doe')).to eql 'Doe, John J.'
     end
 
     it 'should respect limited honorifics' do
