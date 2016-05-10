1d9d3679b72275148d8cc733c77d1247d1b20cb8
Jarod Adair <jarod.adair@manheim.com>
2016-05-10 17:39:52 -0500

    fixing codes to make tests pass

--- namers.rb
+++ namers.rb
@@ -1,7 +1,14 @@
 class Namers
 
   def self.parse_name(name)
+    names = name.split(' ')
+    names.delete_at(0) if self.honorifics.include? names[0]
+    names[-1] = "#{names[-1]},"
+    names.rotate!(names.length - 1).join(' ')
+  end
 
+  def self.honorifics
+    %w(Mr Mr. Ms Ms. Mrs Mrs. Dr Dr.)
   end
 
 end
