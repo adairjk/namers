dfad357c1f8897b36031d565ac324cfcd3388dda
Jarod Adair <jarod.adair@manheim.com>
2016-05-23 16:58:50 -0500

    re-doing and making things pass

--- spec/splitter_spec.rb
+++ spec/splitter_spec.rb
@@ -13,6 +13,56 @@ describe(Splitter) do
     expect(Splitter.new('x').split_lines(2)).to eql "x"
   end
 
+  it('should return the string if equal to the split length') do
+    expect(Splitter.new('x').split_lines(1)).to eql "x"
+  end
+
+  it('should handle empty strings') do
+    expect(Splitter.new('').split_lines(1)).to eql ''
+  end
+
+  it('should split words if longer than split length') do
+    expect(Splitter.new('xxx').split_lines(1)).to eql 'x\nx\nx'
+  end
+
+  it('should insert a new line between words') do
+    expect(Splitter.new('xxx xxx').split_lines(3)).to eql 'xxx\nxxx'
+  end
+
+  it('should insert a new line between words') do
+    expect(Splitter.new('x xxx').split_lines(3)).to eql 'x\nxxx'
+  end
+
+  describe('helper methods') do
+
+    let(:splitter) { Splitter.new('xxxxxxx')}
+
+    describe('split_word') do
+      it('should split a word into lines up to the split length') do
+        splitter.line_length = 3
+        splitter.split_word('xxxxxxx')
+        expect(splitter.lines).to eql ['xxx', 'xxx']
+      end
+
+      it('should put remaining letters in the current line') do
+        splitter.line_length = 3
+        splitter.split_word('xxxxxxx')
+        expect(splitter.line).to eql ['x']
+      end
+    end
+
+  end
+
+  describe('oscars tests') do
+
+    it { expect(Splitter.new(nil).split_lines(1)).to eql('') }
+    it { expect(Splitter.new('').split_lines(1)).to eql('') }
+    it { expect(Splitter.new('x').split_lines(1)).to eql('x') }
+    it { expect(Splitter.new('xx').split_lines(1)).to eql('x\nx') }
+    it { expect(Splitter.new('xxx').split_lines(1)).to eql('x\nx\nx') }
+    it { expect(Splitter.new('x xx').split_lines(1)).to eql('x\nx\nx') }
+
+  end
 
 
   # it('line length should be no longer than requested length') do
@@ -27,11 +77,6 @@ describe(Splitter) do
   #   expect(some_string.splitter(10)).to eql "Four score\nand seven\nyears ago"
   # end
   #
-  # it { expect(splitter.split_lines(1)).to eql('') }
-  # it { expect(nil.splitter(1)).to eql('') }
-  # it { expect('x'.splitter(1)).to eql('x') }
-  # it { expect('xx'.splitter(1)).to eql("x\nx") }
-  # it { expect('xxx'.splitter(1)).to eql("x\nx\nx") }
-  # it { expect('x xx'.splitter(3)).to eql("x\nxx") }
+
 
 end
--- splitter.rb
+++ splitter.rb
@@ -1,25 +1,59 @@
 require 'pry'
+
 class Splitter
 
-    attr_accessor :lines, :line, :string
+    attr_accessor :lines, :line, :string, :words, :line_length
 
     def initialize(string)
       @string = string
     end
 
     def split_lines(line_length)
-      @string if @string.length <= line_length
+      @line_length = line_length
+      return '' if @string.nil?
+      return @string if @string.length <= line_length
+      words.each do |word|
+        if join_line.length + word.length > line_length
+          self.lines << join_line unless line.empty?
+          split_word(word) if word.length > line_length
+          self.line = [word] unless word.empty?
+        else
+          self.line << word unless word.empty?
+        end
+      end
+      @lines << @line unless @line.empty?
+      join_lines
+    end
+
+    def words
+      @words = @string.split
+    end
+
+    def split_word(word)
+      until word.length < @line_length do
+        self.lines << word.slice!(/.{#{@line_length}}/)
+      end
+      self.line = word.empty? ? [] : [word]
+    end
+
+    def join_lines
+      self.lines.empty? ? '' : self.lines.join('\n')
+    end
+
+    def lines
+      @lines ||= []
+    end
+
+    def join_line
+      self.line.empty? ? '' : self.line.join(' ')
+    end
+
+    def line
+      @line ||= []
     end
 
 
 end
-
-
-
-
-
-
-
 
 # def split_lines(length)
 #   return self if self.length < length
